eaf4e33ede3f88e10ed33b2c58aca31d6fdcf06d
Port0goose <diogolessareis2001@gmail.com>
2022-10-14 21:13:25 +0100

    lab4 part 1

--- GE1 Examples 2022/Assets/AITank.cs
+++ GE1 Examples 2022/Assets/AITank.cs
@@ -6,16 +6,38 @@ using UnityEngine;
 public class AITank : MonoBehaviour {
 
     public float radius = 10;
-    public int numWaypoints = 5;
+    public int numWaypoints = 6;
     public int current = 0;
     List<Vector3> waypoints = new List<Vector3>();
     public float speed = 10;
-    public Transform player;    
+    public Transform player; 
+    public float explosionRadius = 1.0f;
+    public int rings = 1;
+
 
     public void OnDrawGizmos()
     {
         if (!Application.isPlaying)
         {
+            float theta = Mathf.PI * 2.0f / (float) numWaypoints;
+            for(int ring = 1; ring < rings ; ring ++)
+            {
+                int count = Mathf.RoundToInt(Mathf.PI * 2.0f * ring);
+                
+                for(int i = 0 ; i < count; i ++)
+                {
+                    Vector3 pos = new Vector3(
+                        Mathf.Cos(theta * i) * radius,
+                        0,
+                        Mathf.Sin(theta * i) * radius
+                    );
+                    pos = transform.TransformPoint(pos);
+                    Gizmos.color = Color.white;
+                    Gizmos.DrawWireSphere(pos, explosionRadius);
+                    //dod.transform.parent = this.transform;
+                }
+
+            }
             // Task 1
             // Put code here to draw the gizmos
             // Use sin and cos to calculate the positions of the waypoints 
@@ -26,7 +48,8 @@ public class AITank : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Awake () {
+    void Awake () 
+    {
         // Task 2
         // Put code here to calculate the waypoints in a loop and 
         // Add them to the waypoints List
